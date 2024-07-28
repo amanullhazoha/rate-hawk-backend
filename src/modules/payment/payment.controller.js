9020fed33e3b275beb9e8961ab2b465081e5a034
amanullha <amanullhazoha3784@gmail.com>
2024-07-29 00:05:29 +0600

    stripe webhook

--- src/modules/payment/payment.controller.js
+++ src/modules/payment/payment.controller.js
@@ -11,21 +11,25 @@ const encodedCredentials = btoa(`${username}:${password}`);
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const orderFinish = async (payload) => {
-  const data = await axios.post(
-    "https://api.worldota.net/api/b2b/v3/hotel/order/booking/finish/",
-    payload,
-    {
-      headers: {
-        Authorization: `Basic ${encodedCredentials}`,
-        "Content-Type": "application/json",
+const orderFinish = async (payload, res) => {
+  try {
+    const data = await axios.post(
+      "https://api.worldota.net/api/b2b/v3/hotel/order/booking/finish/",
+      payload,
+      {
+        headers: {
+          Authorization: `Basic ${encodedCredentials}`,
+          "Content-Type": "application/json",
+        },
       },
-    },
-  );
+    );
 
-  console.log(data, "order finish");
+    console.log(data, "order finish");
 
-  return data;
+    return data;
+  } catch (error) {
+    res.status(200).json({ message: "There was a problem!" });
+  }
   // if (data?.data?.status === "error") throw badRequest(data?.data?.error);
 };
 
@@ -149,7 +153,7 @@ const stripeWebHook = async (req, res, next) => {
       },
     });
 
-    const data = await orderFinish(orderFinishData);
+    const data = await orderFinish(orderFinishData, res);
 
     if (data?.data?.status === "error") throw badRequest(data?.data?.error);
 
