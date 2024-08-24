6ada096ee1932bcecf507e0ae9fe702d6e2ebc38
amanullhazoha <amanullhazoha3784@gmail.com>
2024-08-24 09:58:49 +0600

    json file size issue solve

--- src/config/lib/express.js
+++ src/config/lib/express.js
@@ -47,8 +47,8 @@ module.exports = async () => {
 
   app.set("trust proxy", true);
 
-  app.use(express.json({ limit: "50mb" }));
-  app.use(express.urlencoded({ limit: "50mb", extended: true }));
+  app.use(express.json({ limit: "20mb" }));
+  app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
   app.options("*", cors(corsOptions));
 
